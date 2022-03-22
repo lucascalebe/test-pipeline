1a20b118f363b52a84dffe957c77d820d5004910
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-22 11:36:14 -0300

    :white_check_mark: generating userService class test

---
+++ src/test/java/com/example/testpipeline/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.testpipeline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void validateUserExists() {
+    }
+}
