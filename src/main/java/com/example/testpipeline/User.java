1640e79aee51ee7ca2411520c0c095c1e22dc5d3
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-22 11:33:44 -0300

    :sparkles: generating userClass

---
+++ src/main/java/com/example/testpipeline/User.java
@@ -0,0 +1,12 @@
+package com.example.testpipeline;
+
+public class User {
+
+    private final String name;
+    private final String lastName;
+
+    {
+        this.name = "Lucas";
+        this.lastName = "Calebe";
+    }
+}
