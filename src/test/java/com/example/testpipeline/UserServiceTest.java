26eb3c2c3b908f10466a608b83e6578a728c31fb
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-22 12:11:51 -0300

    :white_check_mark: adding tests

--- src/test/java/com/example/testpipeline/UserServiceTest.java
+++ src/test/java/com/example/testpipeline/UserServiceTest.java
@@ -1,12 +1,21 @@
 package com.example.testpipeline;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserService.class)
 class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
 
     @Test
     void validateUserExists() {
+        Assertions.assertTrue(userService.validateUserExists(new User()));
     }
 }
