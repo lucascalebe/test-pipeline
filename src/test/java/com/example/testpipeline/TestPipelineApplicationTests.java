fda8a5e6cb6b3ea31dd3b32295dee1bbfa8a476b
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-22 11:00:54 -0300

    :tada:

---
+++ src/test/java/com/example/testpipeline/TestPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
