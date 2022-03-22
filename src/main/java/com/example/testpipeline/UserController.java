e3820ba270135306ab30676cec205da4978a4b5c
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-22 11:37:00 -0300

    :sparkles: user controller

---
+++ src/main/java/com/example/testpipeline/UserController.java
@@ -0,0 +1,17 @@
+package com.example.testpipeline;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user")
+    public boolean getUser() {
+        return userService.validateUserExists(new User());
+    }
+}
