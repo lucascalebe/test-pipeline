7d02ada461c50058464d07c1d3039da4d582dbf0
Lucas Calebe <lucascalebe07@hotmail.com>
2022-03-22 11:34:00 -0300

    :sparkles: generating userService class

---
+++ src/main/java/com/example/testpipeline/UserService.java
@@ -0,0 +1,11 @@
+package com.example.testpipeline;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public boolean validateUserExists(User user) {
+        return user != null;
+    }
+}
